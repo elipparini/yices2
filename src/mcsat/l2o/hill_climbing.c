45a454be41d17f4e2e3da4c90c59cf18ed043441
Thomas Hader <tom@hader.email>
2025-02-28 23:14:50 +0100

    fsjump real only

--- src/mcsat/l2o/hill_climbing.c
+++ src/mcsat/l2o/hill_climbing.c
@@ -207,7 +207,7 @@ bool optimize_fs(l2o_t *l2o, term_t t, l2o_search_state_t *state, uint32_t v, do
   for (int i = 0; i < fs->size; ++i) {
     const lp_interval_t *interval = &fs->intervals[i];
     lp_interval_pick_value(interval, &lp_val);
-    if (!lp_value_is_integer(&lp_val)) {
+    if (!lp_value_is_rational(&lp_val)) {
       continue;
     }
     *val = lp_value_to_double(&lp_val);
